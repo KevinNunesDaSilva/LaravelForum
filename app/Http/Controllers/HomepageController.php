5dbd30951617f6ca5dd7edf59abb819d0d120430
Kevin Nunes da Silva <kmh.dasilva@gmail.com>
2021-12-02 19:56:59 +0100

    authentication/homepage added

---
+++ app/Http/Controllers/HomepageController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\homepage;
+use Illuminate\Http\Request;
+
+class HomepageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\homepage  $homepage
+     * @return \Illuminate\Http\Response
+     */
+    public function show(homepage $homepage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\homepage  $homepage
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(homepage $homepage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\homepage  $homepage
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, homepage $homepage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\homepage  $homepage
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(homepage $homepage)
+    {
+        //
+    }
+}
